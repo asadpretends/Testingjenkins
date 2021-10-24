8f363d8ccc9a27d742f17f2734453171a8978c88
Asad Khalid <asad69khalid@gmail.com>
2021-10-25 00:40:36 +0500

    First commit

---
+++ src/test/java/CustomersTest.java
@@ -0,0 +1,25 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CustomersTest {
+
+    private Customers object;
+    @Before
+    public void createobject()
+    {
+        object = new Customers("Umair","Dab no 2",(long)1234,(long)1234567);
+    }
+    @Test
+    public void Testchecktwoaccounts()
+    {
+        Assert.assertFalse(object.checktwoaccounts());
+    }
+    @Test
+    public void Test2checktwoaccounts()
+    {
+        object.Accountnumber.add((long)223344);
+        Assert.assertTrue(object.checktwoaccounts());
+    }
+}
